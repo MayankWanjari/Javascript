03769dcc426f45c1dd5b6b8d9db484d68c57f02b
Mayank <mayankwanjari45@gmail.com>
2025-05-26 21:44:16 +0530

    Javascript topics - strings

---
+++ string.js
@@ -0,0 +1,26 @@
+
+var str = 'string';
+console.log(str.charAt(999)); // ''
+var str = 'string';
+console.log(str.charAt(0)); // s
+//If no character is found, the method returns an empty string.
+
+var word = "Ocygen";
+var newName = word.replace('c', 'x');
+console.log(newName);
+
+var originalString = "Hello, World!";
+var slicedString = originalString.slice(7, 12);
+
+console.log(slicedString); // World
+
+let originalString = "Hello, World!";
+let slicedString = originalString.slice(7);
+
+console.log(slicedString); // World!
+
+
+let originalString = "Hello, World!";
+let slicedString = originalString.slice(-6, -1);
+
+console.log(slicedString); // World
